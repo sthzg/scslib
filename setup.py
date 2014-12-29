2575497e91f7d0b022a4fc15404c0f3f465376b4
sthzg <sthzg@gmx.net>
2014-12-29 12:44:28 +0100

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from distutils.core import setup
+
+setup(
+    name='scslib',
+    version='0.1dev',
+    packages='scslib',
+    license='MIT',
+    long_description=open('README.md').read(),
+)
