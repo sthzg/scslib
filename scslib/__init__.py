145b1d91879c9c5bc8f763da32eeb24f7fdd6e57
sthzg <sthzg@gmx.net>
2014-12-30 16:34:27 +0100

    Package -- Added register_shortcodes() and register_bundle() to package scope.

--- scslib/__init__.py
+++ scslib/__init__.py
@@ -2,4 +2,10 @@
 from __future__ import absolute_import, unicode_literals
 from .utils import (get_tag_names_on_whitelist, camelize)
 from .scslib import Transformer, ShortcodeBase
-from .registry import registered_shortcodes, registered_bundles
+
+from .registry import (
+    registered_shortcodes,
+    registered_bundles,
+    register_bundle,
+    register_shortcodes
+)
