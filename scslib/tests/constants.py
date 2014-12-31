fd7dd027e81aadc5120ea21ae872ec453b833787
sthzg <sthzg@gmx.net>
2014-12-31 15:11:17 +0100

    Tests -- Updated transformer tests.
    Simplified test strings. Added test to check that the transformer's soup equals the soup from the input string.

--- scslib/tests/constants.py
+++ scslib/tests/constants.py
@@ -7,31 +7,18 @@ WHITELIST = ('test', 'image',)
 
 TEST_STRING = """
 <div>
-<h1>Hello world</h1>
-<image embed-id="foobar" class="img-responsive" />
-<test />
-<bs-row fluid="true">
-    <bs-col sm="12" md="8">
-        <h2>Head</h2>
-        <p>Lorem ipsum.</p>
-        <test class="hello-world">Foobar</test>
-    </bs-col>
-    <bs-col sm="12" md="4">
-        <image embed-id="foobar" class="img-responsive" lazy="true" />
-        <h2>Head 2</h2>
-        <p>Dolor sit.</p>
-    </bs-col>
-</bs-row>
-<bs-slideshow autoplay="false">
-    <image embed-id="bam" />
-    <image embed-id="baz" />
-    <image embed-id="bar" />
-</bs-slideshow>
-<crazy-box template="darkblue">
-    <p>Lorem ipsum.</p>
-    <image embed-id="fjord" />
-</crazy-box>
+    <test />
+    <div>
+        <test class="foobar"></test>
+    </div>
 </div>
 """
 
-TEST_STRING_EMPTY = ""
+TEST_STRING_RESULT = """
+<div>
+    <b>Test transformed.</b>
+    <div>
+        <b>Test transformed.</b>
+    </div>
+</div>
+"""
--- scslib/tests/test_transformer.py
+++ scslib/tests/test_transformer.py
@@ -2,16 +2,30 @@
 from __future__ import absolute_import, unicode_literals
 import unittest
 import scslib
+from bs4 import BeautifulSoup
 from scslib.tests.constants import *
 
 
 class TransformerTestCase(unittest.TestCase):
+    def test_markup_in_soup(self):
+        """Soup HTML in transformer is equal to source HTML."""
+        soup = BeautifulSoup(TEST_STRING)
+        transformer = scslib.Transformer(TEST_STRING)
+        self.assertEqual(soup.prettify(), transformer.soup.prettify())
+
     def test_collect_shortcode_tokens(self):
         """Tags that are not on the whitelist are filtered from ``tokens``."""
         transformer = scslib.Transformer(TEST_STRING)
         transformer.collect_shortcode_tokens()
-        self.assertEqual(len(transformer.shortcodes), 8)
+        self.assertEqual(len(transformer.shortcodes), 2)
 
+    def test_build_output(self):
+        """Output of test transformation is correct."""
+        transformer = scslib.Transformer(TEST_STRING)
+        transformer.collect_shortcode_tokens()
+        transformer.build_output()
+        expected = BeautifulSoup(TEST_STRING_RESULT).prettify()
+        self.assertEqual(expected, transformer.soup.prettify())
 
 if __name__ == '__main__':
     unittest.main()
